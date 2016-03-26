886c91aedd6e6896ee924f2d9874712785873192
arianaolson419 <ariana.olson@students.olin.edu>
2016-03-26 19:41:48 -0400

    working fft plots

--- fft_audio_practice.py
+++ fft_audio_practice.py
@@ -3,12 +3,12 @@ from scipy.io import wavfile
 
 sampFreq, snd = wavfile.read('440_sine.wav')
 s1 = snd[:,0]
-
-timeArray = arange(0, 5060.0, 1)
+print sampFreq
+timeArray = arange(0, float(len(s1)), 1)
 timeArray = timeArray / sampFreq
 timeArray = timeArray * 1000 #scale to milliseconds
 
-plt = plot(timeArray, s1, color='k')
+plt = plot(timeArray, s1[0:len(timeArray)], color='k')
 ylabel('Amplitude')
 xlabel('Time (ms)')
 show(plt)
