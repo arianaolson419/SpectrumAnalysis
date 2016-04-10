80f29f12df4726cea25f767658844136d2015237
arianaolson419 <ariana.olson@students.olin.edu>
2016-04-10 18:39:41 -0400

    I got the button to work!!!

--- Web App/page.py
+++ Web App/page.py
@@ -5,10 +5,11 @@ app = Flask(__name__)
 def selection_page():
 	# return "Hello"
 	return render_template('index.html')
-@app.route('/play_song.html', methods=['GET', 'POST'])
+@app.route('/play_song', methods=['GET', 'POST'])
 def test(song=None):
 	song = request.form['song']
 	print song
+	return render_template('play_song.html', song=song)
 
 if __name__ == '__main__':
 	app.run()
