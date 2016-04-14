1b7051f780064b21395d829b63d202e429b9b274
arianaolson419 <ariana.olson@students.olin.edu>
2016-04-14 14:27:20 -0400

    took out redundancies

--- Web App/page.py
+++ Web App/page.py
@@ -1,20 +1,21 @@
 from flask import *
 import sys
-import subprocess
 app = Flask(__name__)
 
 @app.route('/')
 def selection_page():
-	# return "Hello"
-	return render_template('index.html')
+	'''Displays the initial page with the big red button (index.html)'''
+	return render_template('index.html') #displays the button page
 @app.route('/play_song', methods=['GET', 'POST'])
 def test(song=None):
-	song = request.form['song']
-	sys.argv = ['AV_FFT.py', '/home/arianaolson/SpectrumAnalysis/02_White_Freckles.wav']
-	execfile('AV_FFT.py')
-	# subprocess.call([sys.executable, 'test.py', '1'])
-	# subprocess.call([sys.executable, 'AV_FFT.py', '/home/arianaolson/SpectrumAnalysis/02_White_Freckles.wav'])
-	return render_template('play_song.html', song=song)
+	'''displays the page letting the user know what song is being played and visualized (play_song.html). 
+	Also runs a python script with arguments on the computer'''
+	song = request.form['song'] 
+	sys.argv = ['test.py', 'Ariana', 1, 'O']
+	execfile(sys.argv[0])
+	# sys.argv = ['AV_FFT.py', '/home/arianaolson/SpectrumAnalysis/02_White_Freckles.wav'] #set system arguments
+	# execfile('AV_FFT.py') #executes the script. file name must == sys.argv[0]
+	return render_template('play_song.html', song=song) #displays the play_song page
 
 if __name__ == '__main__':
 	app.run()
