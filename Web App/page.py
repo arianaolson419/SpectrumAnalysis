02015699c7f3d1b046d26e50e18a75a9fffbe2b9
arianaolson419 <ariana.olson@students.olin.edu>
2016-04-19 15:04:56 -0400

    restructuring to play songs. not working yet

--- Web App/page.py
+++ Web App/page.py
@@ -1,7 +1,9 @@
 from flask import *
-import sys
+import os, sys
+import playsong
 app = Flask(__name__)
 
+songs = {'song1': ['White_Freckles.wav']}
 @app.route('/')
 def selection_page():
 	'''Displays the initial page with the big red button (index.html)'''
@@ -11,11 +13,8 @@ def test(song=None):
 	'''displays the page letting the user know what song is being played and visualized (play_song.html). 
 	Also runs a python script with arguments on the computer'''
 	song = request.form['song'] 
-	sys.argv = ['test.py', 'Ariana', 1, 'O']
-	execfile(sys.argv[0])
-	# sys.argv = ['AV_FFT.py', '/home/arianaolson/SpectrumAnalysis/02_White_Freckles.wav'] #set system arguments
-	# execfile('AV_FFT.py') #executes the script. file name must == sys.argv[0]
+	playsong.playit('Gooey.wav')
 	return render_template('play_song.html', song=song) #displays the play_song page
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
