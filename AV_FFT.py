90df9a43ac20649dd5a61ed6c3556d712688088e
hmowen <harper.owen@students.olin.edu>
2016-04-20 12:04:53 -0400

    updated FFT file & new LED control program

--- AV_FFT.py
+++ AV_FFT.py
@@ -16,120 +16,146 @@ import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pylab
 import matplotlib.pyplot as plt
+import subprocess as sp
 
-print 'testing...'
-song_file = '02_White_Freckles.wav' 		# song_file must be a .wav file
+# song_file must be a .wav file
+song_file = '/home/harper/SpectrumAnalysis/Gooey.wav'
 song_name = str(os.path.splitext(song_file.split('/')[-1])[0])
 
 
-if __name__ == '__main__':
+""" Open the file and get attributes """
+# open wave file as read only
+song = wave.open(song_file, 'r')
+# number of individual data snippets (frames) in song
+frames = song.getnframes()
+# frequency of frames per second
+frame_rate = song.getframerate()
+# given discretized signal, get the width of each portion (sample)
+sample_width = song.getsampwidth()
+# determine the length of the song by dividing frames by frequency
+song_length = frames / float(frame_rate)
+channels = song.getnchannels()
+# reads & returns each frame of audio as a string of bytes
+song_data = song.readframes(frames)
 
-	""" Open the file and get attributes """
-	song = wave.open(song_file, 'r')			# open wave file as read only
-	frames = song.getnframes()					# number of individual data snippets (frames) in song
-	frame_rate = song.getframerate()			# frequency of frames per second
-	print "Frames: %s" % frames
-	sample_width = song.getsampwidth()			# given discretized signal, get the width of each portion (sample)
-	song_length = frames / float(frame_rate)	# determine the length of the song by dividing frames by frequency
-	channels = song.getnchannels()
-	print "Channels: %s"
-	song_data = song.readframes(frames)			# reads & returns each frame of audio as a string of bytes
+song.close()
 
-	song.close()								# close song file, we don't need it anymore
-
-
-	""" Unpack the binary data from 
-		song_data into an array """
-	song_data = struct.unpack('{}h'.format(frames*2), song_data)
-
-	
-	""" Initialize FFT variables """
-	fourier_rate = 24							# frames per second
-	fourier_width = 1.0/fourier_rate			# width of each discrete fourier
-	fourier_index = \
-		fourier_width * float(frame_rate)		# index is width times frame frequency
-
-	total_transforms = \
-		int(round(song_length*fourier_rate))	# determines total number of fourier transforms by multiplying the song length by the fourier rate
-	fourier_spacing = \
-		round(fourier_width*float(frame_rate))	# determines which points at which discretize fourier transform
-
-	last_pt = \
-		int(round(song_length*float(frame_rate) + fourier_index))
-
-	freq = frame_rate / fourier_index * np.arange(fourier_index)
-
-	band_width = 1.0 / fourier_width			# determine band width
-	
-	fft_averages = [] 							# initialize an array for fft averages
-	all_fft_avgs = [] 							# initialize an array to contain all ffts over all frames
+""" Unpack the binary data from 
+	song_data into an array """
+song_data = struct.unpack('{}h'.format(frames*2), song_data)
 
 
-	""" Convert Frequency (input) to Index (output) """
-	def FreqToIndex(f):
-		# if f (frequency) is lower than bandwidth of spectrum[0], return 0
-		if f < band_width/2:
-			return 0
-		# if f is larger than 1/2 the frame rate minus 1/2 the band width, return fourier index - 1
-		if f > (frame_rate/2) - band_width/2:
-			return fourier_index - 1
-		fraction = float(f) / float(frame_rate)	
-		index = round(fourier_index * fraction)	# converted frequency to index
-		return index
+""" Initialize FFT variables """
+# frames per second
+fourier_rate = 24
+# width of each discrete fourier
+fourier_width = 1.0/fourier_rate
+# index is width times frame frequency
+fourier_index = fourier_width * float(frame_rate)
+
+# determines total number of fourier transforms by multiplying the song length by the fourier rate
+total_transforms = int(round(song_length*fourier_rate))
+# determines which points at which discretize fourier transform
+fourier_spacing = round(fourier_width*float(frame_rate))
+
+last_pt = int(round(song_length*float(frame_rate) + fourier_index))
+
+freq = frame_rate / fourier_index * np.arange(fourier_index)
+
+# determine band width
+band_width = 1.0 / fourier_width
+
+# initialize an array for fft averages
+fft_averages = []
+# initialize an array to contain all ffts over all frames
+all_fft_avgs = []
+
+
+""" Convert Frequency (input) to Index (output) """
+def FreqToIndex(f):
+	# if f (frequency) is lower than bandwidth of spectrum[0], return 0
+	if f < band_width/2:
+		return 0
+	# if f is larger than 1/2 the frame rate minus 1/2 the band width, return fourier index - 1
+	if f > (frame_rate/2) - band_width/2:
+		return fourier_index - 1
+	fraction = float(f) / float(frame_rate)
+	# converted frequency to index
+	index = round(fourier_index * fraction)
+	return index
 
 
 
-	""" Average the frequencies in each band """
-	def AvgFftBands(fft_array):
-		num_bands = 12							# num_bands based on frequency bands (12 notes in 1 octave)
-		del fft_averages[:]						# reinitialize an empty fft_averages list
-		for band in range(0, num_bands):
-			avg = 0.0
+""" Average the frequencies in each band """
+def AvgFftBands(fft_array):
+	# num_bands based on frequency bands (12 notes in 1 octave)
+	num_bands = 12
+	# reinitialize an empty fft_averages list
+	del fft_averages[:]
+	for band in range(0, num_bands):
+		avg = 0.0
 
-			if band == 0:
-				lowFreq = int(0)
-			else:
-				lowFreq = int(int(frame_rate / 2) / float(2 ** (num_bands - band)))
-			hiFreq = int((frame_rate / 2) / float(2 ** ((num_bands-1) - band)))
-			lowBound = int(FreqToIndex(lowFreq))
-			hiBound = int(FreqToIndex(hiFreq))
-			for j in range(lowBound, hiBound):
-				avg += fft_array[j]
+		if band == 0:
+			lowFreq = int(0)
+		else:
+			lowFreq = int(int(frame_rate / 2) / float(2 ** (num_bands - band)))
+		hiFreq = int((frame_rate / 2) / float(2 ** ((num_bands-1) - band)))
+		lowBound = int(FreqToIndex(lowFreq))
+		hiBound = int(FreqToIndex(hiFreq))
+		for j in range(lowBound, hiBound):
+			avg += fft_array[j]
 
-			avg /= (hiBound - lowBound + 1)
-			fft_averages.append(avg)
+		avg /= (hiBound - lowBound + 1)
+		fft_averages.append(avg)
 
-	
-	# for offset in range(0, total_transforms):
-	# 	start = int(offset * fourier_index)
-	# 	end = int((offset * fourier_index) + fourier_index -1)
+def RemapFftInterval(fft_array):
+	bar_vals = []
+	mean = np.mean(fft_array)
+	stnd = np.std(fft_array)
+	output_max = mean + stnd*3
+	for i in range(len(fft_array)-1):
+		ratio = output_max / 25
+		new_val = np.ceil((fft_array[i] / ratio))
+		if new_val >= 25:
+			new_val = 25
+			bar_vals.append(new_val)
+		elif 1 <= new_val < 25: 
+			bar_vals.append(new_val)
+		else:
+			bar_vals.append(0)
+	return bar_vals
 
-	# 	print "Processing sample %i of %i (%d seconds)" % (offset + 1, total_transforms, end/float(frame_rate))
-	# 	sample_range = song_data[start:end]
+for offset in range(0, total_transforms):
+	start = int(offset * fourier_index)
+	end = int((offset * fourier_index) + fourier_index -1)
 
-		# """ This is where we actually use FFT """
-		# fft_data = abs(np.fft.fft(sample_range))
+	sample_range = song_data[start:end]
 
-		# fft_data *= ((2**.5)/fourier_index)	# normalize it a second time to make numbers sensible
-		# AvgFftBands(fft_data)
-		# bar_levels = fft_averages
-		# bar_levels = np.transpose(bar_levels)
-		# all_fft_avgs.append(bar_levels)
+	""" This is where we actually use FFT """
+	fft_data = abs(np.fft.fft(sample_range))
 
-		# """ Uncomment this if you want to make bar graphs """
-		# x_axis = range(0,12)
-		# y_axis = fft_averages
-		# width = 0.35
-		# p1 = plt.bar(x_axis, y_axis, width, color='r')
-		# print next
+	fft_data *= ((2**.5)/fourier_index)	# normalize it a second time to make numbers sensible
+	AvgFftBands(fft_data)
+	bar_levels = RemapFftInterval(fft_averages)
+	bar_levels = np.transpose(bar_levels)
+	all_fft_avgs.append(bar_levels)
 
-		# filename = str('frame_%05d' % offset) + '.png'
-		# plt.savefig(filename, dpi=100)
-		# plt.close()
+	""" Uncomment this if you want to make bar graphs """
+	# x_axis = range(0,12)
+	# y_axis = fft_averages
+	# width = 0.35
+	# p1 = plt.bar(x_axis, y_axis, width, color='r')
+	# print next
 
+	# filename = str('frame_%05d' % offset) + '.png'
+	# plt.savefig(filename, dpi=100)
+	# plt.close()
 
-	""" Uncomment this if you want to save FFT Data to a CSV """
-	# filename = str(song_name) + 'Data' + '.csv'
-	# np.savetxt(filename, all_fft_avgs, delimiter=",")
+	# command1 = ['/usr/bin/ffmpeg' '-f' 'image2' '-r' '24' '-i' 'frame_%05d.png' '-vcodec mpeg4' '-y' 'movie.mp4']
+	# pipe = sp.Popen(command1, stdin=sp.PIPE,stdout=sp.PIPE, stderr=sp.PIPE)
 
-	print "Done!"
+""" Uncomment this if you want to save FFT Data to a CSV """
+filename = str(song_name) + 'Data' + '.csv'
+np.savetxt(filename, all_fft_avgs, delimiter=",")
+
+print "Done!"
---
+++ AV_LED.py
@@ -0,0 +1,89 @@
+import time
+import numpy as np
+
+""" Program to Control LEDs """
+
+# LED strip configuration:
+LED_COUNT      = 150     # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (must support PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 5       # DMA channel to use for generating signal (try 5)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+
+
+""" Import FFT File """
+song_dir = "/home/harper/SpectrumAnalysis/GooeyData.csv"
+bar_array = np.genfromtxt(song_dir, delimiter=',', dtype=None, names=True)
+
+
+""" Run LEDs """
+def AudioVisualizer(strip, color, led_array, LED_COUNT):
+ 	fps = 24
+ 	q = LED_COUNT * 2 / 12
+	for frame in range(len(led_array)-1):
+		for i in frame:
+			if i == 0:
+				for j in range(i[0]-1):
+					strip.setPixelColor(j+1, color)
+					strip.show()
+			elif i == 1:
+				for j in range(i[1]-1):
+					strip.setPixelColor(2*q-j, color)
+					strip.show()
+			elif i == 2:
+				for j in range(i[2]-1):
+					strip.setPixelColor(2*q+1+j, color)
+					strip.show()
+			elif i == 3:
+				for j in range(i[3]-1):
+					strip.setPixelColor(4*q-j, color)
+					strip.show()
+			elif i == 4:
+				for j in range(i[4]-1):
+					strip.setPixelColor(4*q+1+j, color)
+					strip.show
+			elif i == 5:
+				for j in range(i[5]-1):
+					strip.setPixelColor(6*q-j, color)
+					strip.show()
+			elif i == 6:
+				for j in range(i[6]-1):
+					strip.setPixelColor(6*q+1+j, color)
+					strip.show()
+			elif i == 7:
+				for j in range(i[7]-1):
+					strip.setPixelColor(8*q-j, color)
+					strip.show()
+			elif i == 8:
+				for j in range(i[8]-1):
+					strip.setPixelColor(8*q+1+j, color)
+					strip.show()
+			elif i == 9:
+				for j in range(i[9]-1):
+					strip.setPixelColor(10*q-j, color)
+					strip.show()
+			elif i == 10:
+				for j in range(i[10]-1):
+					strip.setPixelColor(10*q+1+j, color)
+					strip.show()
+			elif i == 11:
+				for j in range(i[11]-1):
+					strip.setPixelColor(12*q-j, color)
+					strip.show
+		time.sleep(1/fps)
+
+
+# Main program logic follows:
+if __name__ == '__main__':
+	# Create NeoPixel object with appropriate configuration.
+	strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
+	# Intialize the library (must be called once before other functions).
+	strip.begin()
+
+	print ('Press Ctrl-C to quit.')
+	while True:
+
+	# Begin Animations
+	AudioVisualizer(strip, Color(127, 0, 0), bar_array, LED_COUNT)
+
