e66132d60a495e1335b47fb9c21969e41568cf57
arianaolson419 <ariana.olson@students.olin.edu>
2016-04-20 18:57:10 -0400

    document for second technical review

--- Web App/playsong.py
+++ Web App/playsong.py
@@ -1,14 +1,15 @@
+'''Defining the functions called by the web app (page.py)no'''
 import pygame
 import time
 
 
 def playit(song):
+	'''Plays a given song
+	song: file location of .wav or .mp3 audio file
+	'''
 	pygame.init()
-
 	pygame.mixer.music.load(song)
-
 	pygame.mixer.music.play()
-
 	time.sleep(10)
 
 if __name__== '__main__':
