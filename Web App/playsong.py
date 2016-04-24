e192f3419f0ef37e09146d47f91b9e957ee6216e
arianaolson419 <ariana.olson@students.olin.edu>
2016-04-24 15:51:38 -0400

    plays song without pygame -- much faster!

--- Web App/playsong.py
+++ Web App/playsong.py
@@ -1,17 +1,12 @@
 '''Defining the functions called by the web app (page.py)no'''
-import pygame
-import time
+import subprocess
 
 
 def playit(song):
-	'''Plays a given song
-	song: file location of .wav or .mp3 audio file
-	'''
-	pygame.init()
-	pygame.mixer.music.load(song)
-	pygame.mixer.music.play()
-	time.sleep(10)
+	'''Plays a given song'''
+	subprocess.Popen('aplay ' + song, shell=True)
 
 if __name__== '__main__':
 	song = "/home/arianaolson/SpectrumAnalysis/02_White_Freckles.wav"
 	playit(song)
+
