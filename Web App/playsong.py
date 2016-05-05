a87339acc62cf933afa4744afc24c9917e1b6032
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-04 21:23:33 -0400

    subprocess again

--- Web App/playsong.py
+++ Web App/playsong.py
@@ -1,17 +1,12 @@
 '''Defining the functions called by the web app (page.py)no'''
 import subprocess
-import pygame
-import time
 
 def playit(song):
 	'''Plays a given song'''
-	pygame.init()
+	cmd = 'mplayer ' + song
+	popen = subprocess.Popen(cmd, shell=True)
+	popen.communicate()
 
-	pygame.mixer.music.load(song)
-
-	pygame.mixer.music.play()
-
-	time.sleep(10)
 
 if __name__== '__main__':
 	song = "audio/White_Freckles.wav"
