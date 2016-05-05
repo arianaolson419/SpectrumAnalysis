4e290014136c8cf0e7d3bf11c2ecca53889fe220
arianaolson419 <ariana.olson@students.olin.edu>
2016-05-04 20:21:21 -0400

    switched to pygame

--- Web App/playsong.py
+++ Web App/playsong.py
@@ -1,9 +1,17 @@
 '''Defining the functions called by the web app (page.py)no'''
 import subprocess
+import pygame
+import time
 
 def playit(song):
 	'''Plays a given song'''
-	subprocess.Popen('aplay ' + song, shell=True)
+	pygame.init()
+
+	pygame.mixer.music.load(song)
+
+	pygame.mixer.music.play()
+
+	time.sleep(10)
 
 if __name__== '__main__':
 	song = "audio/White_Freckles.wav"
